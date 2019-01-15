e85e6e790a500770f2e3909bea8334c6dfcee25e
Youssef Meskini <youssef@smartsailors.net>
2019-01-15 14:00:34 +0100

    Add config for production and developpment

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
@@ -14,9 +14,14 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-env"]
+          presets: ["@babel/preset-env", "@babel/preset-react"]
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 };
