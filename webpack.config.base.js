00025656a86877e0c84b081236b823ff64f98468
Youssef Meskini <youssef@smartsailors.net>
2019-01-15 14:30:13 +0100

    Add css loader to webpack

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -16,6 +16,11 @@ module.exports = {
           presets: ["@babel/preset-env", "@babel/preset-react"],
           plugins: ["@babel/plugin-proposal-class-properties"]
         }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/
       }
     ]
   },
