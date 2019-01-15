eec09bbdd90e3bf391eea28e8710befe7b5b676c
Youssef Meskini <youssef@smartsailors.net>
2019-01-15 15:51:46 +0100

    Add error boundary class

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import '@babel/polyfill';
 import App from './App';
 import './styles.css';
+import DefaultErrorBoundary from './DefaultErrorBoundary';
+
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
   </StrictMode>,
   document.getElementById('app')
 );
