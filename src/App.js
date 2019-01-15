00c7055781d59362725a3070b41bc285afe741ad
Youssef Meskini <youssef@smartsailors.net>
2019-01-15 15:57:04 +0100

    Clean up for boilerplate

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
-const Warning = React.lazy(() => import('./Warning'));
-
 class App extends Component {
-  state = {
-    counter: 0
-  };
-
-  increment = () => {
-    this.setState(state => ({
-      counter: state.counter + 1
-    }));
-  };
-
-  decrement = () => {
-    this.setState(state => ({
-      counter: state.counter - 1
-    }));
-  };
   render() {
-    const { counter } = this.state;
     return (
       <div>
         <h1>Hello World</h1>
-        <h2>Counter {counter}</h2>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
-        {counter > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
       </div>
     );
   }
--- src/Warning.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Warning = () => <div>Warning</div>;
-
-Warning.displayName = 'Warning';
-
-export default Warning;
