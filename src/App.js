92574cee649d9f763259e1a1bf2437041d48b1a8
Youssef Meskini <youssef@smartsailors.net>
2019-01-15 14:25:42 +0100

    Add simple example to use babel proposal class

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    counter: 0
+  };
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>Count: {this.state.counter}</h2>
+        <button
+          onClick={() =>
+            this.setState(state => ({
+              counter: state.counter + 1
+            }))
+          }
+        >
+          +
+        </button>
+        <button
+          onClick={() =>
+            this.setState(state => ({
+              counter: state.counter - 1
+            }))
+          }
+        >
+          -
+        </button>
+      </div>
+    );
   }
 }
 
