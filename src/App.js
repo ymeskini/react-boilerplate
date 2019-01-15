30bf41bc23488ff70d67df34bb67572164b7fd01
Youssef Meskini <youssef@smartsailors.net>
2019-01-15 17:24:11 +0100

    Get back simple counter to check hot loading

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState(state => ({
+      count: state.count + 1
+    }));
+  };
+
+  decrement = () => {
+    this.setState(state => ({
+      count: state.count - 1
+    }));
+  };
+
   render() {
+    const { count } = this.state;
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Helo world</h1>
+        <h2>Count: {count}</h2>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
       </div>
     );
   }
