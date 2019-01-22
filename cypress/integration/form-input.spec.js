e41da9be88248871aa05643e1424caa684ff061f
Youssef Meskini <youssef@smartsailors.net>
2019-01-22 16:43:00 +0100

    Add e2e tests for todoapp

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -1,7 +1,16 @@
 describe('Form Input', () => {
+  beforeEach(() => {
+    cy.seedAndVisit([]);
+  });
+
   it('Focuses the input on load', () => {
-    cy.visit('/todo');
-    // focus the first element on the page
     cy.focused().should('have.class', 'new-todo');
+  });
+
+  it('Accepts input', () => {
+    const typedText = 'New todo';
+    cy.get('.new-todo')
+      .type(typedText)
+      .should('have.value', typedText);
   });
 });
