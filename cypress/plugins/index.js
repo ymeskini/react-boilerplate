a96d876aa70455594e71c35cd6a0bf70e6f6e557
Youssef Meskini <youssef@smartsailors.net>
2019-01-28 17:53:53 +0100

    Keep cypress in js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,11 +11,4 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const wp = require('@cypress/webpack-preprocessor');
-
-module.exports = on => {
-  const options = {
-    webpackOptions: require('../webpack.config')
-  };
-  on('file:preprocessor', wp(options));
-};
+module.exports = on => {};
